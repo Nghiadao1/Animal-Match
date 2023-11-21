7b203f35305740036c628e17184e2fba9a888188
Dao Trong Nghia <nghiadao200502@gmail.com>
2023-11-21 16:17:48 +0700

    Arrange Animal

--- Match Animal/Assets/Scripts/Animal.cs
+++ Match Animal/Assets/Scripts/Animal.cs
@@ -27,6 +27,7 @@ public class Animal : MonoBehaviour
             isClick = false;
             GameManager.isMatched = false;
         }
+        ArrangeAnimal();
     }
     public void OnMouseDown()
     {
@@ -47,7 +48,29 @@ public class Animal : MonoBehaviour
             }
         }
     }
-    public void ArrangeAnimal(){
-
+    public void ArrangeAnimal()
+    {
+        //check if pos1 not have animal, move animal in pos2 to pos1 and continue with another pos
+        for (int i = 0; i < boardMatchList.listPosMatch.Count; i++)
+        {
+            PositionMatch positionMatch = boardMatchList.listPosMatch[i].GetComponent<PositionMatch>();
+            if (positionMatch.isHaveAnimal == false)
+            {
+                for (int j = i + 1; j < boardMatchList.listPosMatch.Count; j++)
+                {
+                    PositionMatch positionMatch2 = boardMatchList.listPosMatch[j].GetComponent<PositionMatch>();
+                    if (positionMatch2.isHaveAnimal == true)
+                    {
+                        positionMatch.animal = positionMatch2.animal;
+                        positionMatch2.animal = null;
+                        positionMatch2.isHaveAnimal = false;
+                        positionMatch.isHaveAnimal = true;
+                        //update position of animal
+                        positionMatch.animal.transform.position = Vector3.Lerp(positionMatch.animal.transform.position, positionMatch.transform.position, 1f);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
